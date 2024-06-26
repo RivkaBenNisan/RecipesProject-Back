74f58e008b39a2d7c5ff2e24e8c39100a123ccea
Rivka <rbn9574@gmail.com>
2024-06-26 12:29:35 +0300

    recipes project

---
+++ DAL/Interfaces/User.cs
@@ -0,0 +1,13 @@
+﻿using DAL.Models;
+using System;
+using System.Collections.Generic;
+
+namespace DAL.Interfaces;
+
+public interface IUserDal
+{
+   User Register(User user);
+   User Login(string email, string password);
+    List<User> GetAll();
+
+}
